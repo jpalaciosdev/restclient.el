12b0274b2bdd5631adeeadba00293785e2efe4e4
Samuel Culpepper <samuel@samuelculpepper.com>
2021-11-02 16:46:58 +0100

    tweak: remove fontification from buffer-substring
    Signed-off-by: Abdelhak Bougouffa <abougouffa@fedoraproject.org>

--- restclient.el
+++ restclient.el
@@ -610,7 +610,7 @@ bound to C-c C-r."
 	(when restclient-curr-request-functions
 	  (add-hook 'restclient-response-loaded-hook 'restclient-single-request-function))
         (let* ((cmax (restclient-current-max))
-               (entity (restclient-parse-body (buffer-substring (min (point) cmax) cmax) vars))
+               (entity (restclient-parse-body (buffer-substring-no-properties (min (point) cmax) cmax) vars))
                (url (restclient-replace-all-in-string vars url)))
           (apply func method url headers entity args))))))
 
